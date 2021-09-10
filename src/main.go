e23a09a22f813f4a359f17bc8e833c1e9eeec2f0
Tim <tymekch@pm.me>
2021-09-11 01:40:01 +0200

    Basic concurrent connection handling

--- src/main.go
+++ src/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha512"
+	"encoding/binary"
 	"encoding/hex"
-	"fmt"
+	"flag"
+	"log"
 	"math"
+	"math/rand"
+	"net"
+	"os"
 	"time"
 )
 
@@ -55,6 +61,7 @@ func (e EmptyLeaf) Hash() (Hash, error) {
 // tx_hash -> array_index (maybe overoptimization?)
 type MerkleTree struct {
 	nodes []Node
+	depth uint
 }
 
 // NewMerkleTree constructs a binary tree in which
@@ -86,7 +93,7 @@ func NewMerkleTree(elements []Hashable) (*MerkleTree, error) {
 		}
 		tree[i] = Node{leaf: false, Hash: childrenHash, Content: nil}
 	}
-	return &MerkleTree{tree}, nil
+	return &MerkleTree{tree, uint(baseIndex)}, nil
 }
 
 // fillElements rounds the number of leafs to the nearest
@@ -105,9 +112,10 @@ func (m *MerkleTree) RootHash() Hash {
 	return m.nodes[0].Hash
 }
 
-func (m *MerkleTree) PrintTree() string {
-	result := ""
-
+// TODO: Print the MerkleTree in a pretty form with hashes and values
+func (m *MerkleTree) PresentTree() string {
+	//result := ""
+	return ""
 }
 
 type Block struct {
@@ -119,18 +127,92 @@ type Blockchain struct {
 	blocks []Block
 }
 
-func main() {
-	txs := []Hashable{&Transaction{12}, &Transaction{24}, &Transaction{36}}
-	for _, tx := range txs {
-		hash, _ := tx.Hash()
-		fmt.Println(hash.String())
-	}
-	tree, err := NewMerkleTree(txs)
-	if err != nil {
-		panic(err)
-	}
-	for i, el := range tree.nodes {
-		fmt.Println(i, el)
+func HashBlock(block Block) Hash {
+	return make([]byte, 4)
+}
 
+const (
+	connHost = "localhost"
+	connType = "tcp"
+)
+
+func main() {
+	/*
+		txs := []Hashable{&Transaction{12}, &Transaction{24}, &Transaction{36}}
+		for _, tx := range txs {
+			hash, _ := tx.Hash()
+			fmt.Println(hash.String())
+		}
+		tree, err := NewMerkleTree(txs)
+		if err != nil {
+			panic(err)
+		}
+		for i, el := range tree.nodes {
+			fmt.Println(i, el)
+		}
+		fmt.Println(tree.RootHash().String())
+	*/
+
+	// Define custom port as cli param
+	var connPort string
+	var defaultPeers bool
+	flag.StringVar(&connPort, "p", "8080", "Listener port")
+	flag.BoolVar(&defaultPeers, "i", false, "Ignore default peer list on startup")
+	flag.Parse()
+	// Default peer list
+	peers := []string{"localhost:8081", "localhost:8082"}
+
+	if defaultPeers {
+		for _, peer := range peers {
+			if conn, err := net.Dial(connType, peer); err != nil {
+				log.Println("Failed connection to peer", peer, err.Error())
+			} else {
+				go connectToPeer(conn)
+			}
+		}
+
+	}
+
+	log.Printf("Starting %s server on %s:%s\n", connType, connHost, connPort)
+	listener, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		log.Fatalln("Error while opening listener: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+
+	for {
+		if conn, err := listener.Accept(); err != nil {
+			log.Fatalln("Error while accepting connection: ", err.Error())
+		} else {
+			log.Printf("Got peer from %s", conn.RemoteAddr().String())
+			go handlePeerConnection(conn)
+		}
+	}
+
+}
+
+func connectToPeer(conn net.Conn) {
+	for {
+		tx := Transaction{rand.Intn(100)}
+		conn.Write(tx.toBytes())
+		conn.Write([]byte("\n"))
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func handlePeerConnection(conn net.Conn) {
+	for {
+		buffer, err := bufio.NewReader(conn).ReadBytes('\n')
+		if err != nil {
+			log.Println("Terminating connection with client: ", err.Error())
+			conn.Close()
+			return
+		}
+		tx := binary.LittleEndian.Uint32(buffer[:len(buffer)-1])
+		log.Printf("Got message: %v from peer %s", Transaction{int(tx)}, conn.RemoteAddr().String())
+		//conn.Write([]byte("ACC\n"))
+		//time.Sleep(3 * time.Second)
 	}
 }
